371827e501101505d49d890402e9e93563f6048e
nikhilsaxena07 <nikhil.saxena@joshtechnology.com>
2025-06-25 22:27:24 +0530

    added scss styling for footer

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,10 @@
 import '../styles/main.scss';
 import { initNavbarToggle } from './navbar';
 import { initTestimonialSlider } from './testimonial';
+import { handleFooterList } from './footer';
 
 document.addEventListener('DOMContentLoaded', () => {
     initNavbarToggle();
     initTestimonialSlider();
+    handleFooterList();
 });

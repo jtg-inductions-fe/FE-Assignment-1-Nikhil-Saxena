ee1436ed83335b8847c1b090f868c89703c96a60
nikhilsaxena07 <nikhil.saxena@joshtechnology.com>
2025-06-26 15:32:54 +0530

    rebased with main branch and minor fixes

--- src/script/testimonial.js
+++ src/script/testimonial.js
@@ -5,20 +5,7 @@ export function initTestimonialSlider() {
         const splide = new Splide(testimonialCarousel, {
             perPage: 1,
             pagination: true,
-            autoplay: true,
-            arrows: false,
-            interval: 3000,
         });
         splide.mount();
-        document
-            .getElementById('testimonial-prev')
-            .addEventListener('click', () => {
-                splide.go('<');
-            });
-        document
-            .getElementById('testimonial-next')
-            .addEventListener('click', () => {
-                splide.go('>');
-            });
     }
 }
